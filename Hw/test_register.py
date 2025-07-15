41a6399721dffc07e224ac4b9afc2dadc8a9b38b
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2025-07-15 18:09:47 +0600

    pytest

---
+++ Hw/test_register.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+from faker import Faker
+fake = Faker()
+
+def test_registration_valid():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://tutorialsninja.com/demo/index.php?route=account/register")
+    time.sleep(18)
+
+    FirstName = driver.find_element(by=By.ID, value="input-firstname")
+    FirstName.send_keys(fake.first_name())
+
+    LastName = driver.find_element(by=By.ID, value="input-lastname")
+    LastName.send_keys(fake.last_name())
+
+    E_mail = driver.find_element(by=By.ID, value="input-email")
+    E_mail.send_keys(fake.email())
+
+    Telephone = driver.find_element(by=By.ID, value="input-telephone")
+    Telephone.send_keys(fake.phone_number())
+
+    Password = driver.find_element(by=By.ID, value="input-password")
+    Password.send_keys("123456")
+
+    PasswordConfirm = driver.find_element(by=By.ID, value="input-confirm")
+    PasswordConfirm.send_keys("123456")
+
+    Subscribe = driver.find_element(by=By.NAME, value="newsletter")
+    Subscribe.click()
+
+    Privacy_Policy = driver.find_element(by=By.NAME, value="agree")
+    Privacy_Policy.click()
+
+    Continue = driver.find_element(by=By.CSS_SELECTOR, value="input.btn")
+    Continue.click()
